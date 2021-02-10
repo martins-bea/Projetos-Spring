433d37e7913a7e1eb69f33d67a521501c383194e
unknown <beatriz.martins.15@hotmail.com>
2021-02-09 22:02:50 -0300

    criação api devfood delivery

---
+++ devfood/src/main/java/com/devolution/devfood/DevfoodApplication.java
@@ -0,0 +1,13 @@
+package com.devolution.devfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevfoodApplication.class, args);
+	}
+
+}
